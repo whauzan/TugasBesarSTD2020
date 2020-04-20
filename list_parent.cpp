2732f6e8e429878c36d754a0fe05a900e994d129
whauzan <57913209+whauzan@users.noreply.github.com>
2020-04-21 01:48:52 +0700

    Parent setengah jadi
    cek yang aku jadiin comment yak

--- list_parent.cpp
+++ list_parent.cpp
@@ -1,101 +1,171 @@
 #include "list_parent.h"
-#include <iostream>
 
-using namespace std;
-
-void createList_parent(List &L){
-    first(L)=NULL;
+void createList_parent(List_parent &LP){
+    first_parent(L) = NULL;
 }
 
-void newElement_parent(address &P, int id_baru, string nama_mhs, string asal_sekolah, string major, int donasi){
-    address P;
-    P = new elmList;
-    info_parent(P).ID = id_baru;
-    info_parent(P).nama = nama_mhs;
-    info_parent(P).sma = asal_sekolah;
-    info_parent(P).jurusan_sma = major;
-    info_parent(P).sumbangan = donasi;
-    prev(P)=NULL;
-    next_parent(P)=NULL;
+address_parent newElement_parent(parent x){
+    address_parent P = new elmList;
+    info_parent(P) = x
+    prev_parent(P) = NULL;
+    next_parent(P) = NULL;
 }
 
-void insertFirst_parent(List &L, address P){
-    if (next_parent(first(L))!=prev(first(L))) {
-        next_parent(P)=first(L);
-        prev(P)=prev(first(L));
-        next_parent(prev(first(L)))=P;
-        prev(first(L))=P;
-        first(L)=P
+void deallocate_parent(address_parent &P){
+    delete P;
+}
+
+void insertFirst_parent(List_parent &LP, address_parent P){
+    if (next_parent(first_parent(LP)) != prev_parent(first_parent(LP))) { //kenapa kondisinya ribet amat si :3 cek yang di bawah coba
+        next_parent(P) = first_parent(LP);
+        prev_parent(P) = prev_parent(first_parent(LP));
+        next_parent(prev_parent(first_parent(LP))) = P;
+        prev_parent(first_parent(LP)) = P;
+        first_parent(L) = P;
     } else {
-        first(L)=P;
-        prev(P)=P;
-        next_parent(P)=P;
+        first_parent(LP) = P;
+        prev_parent(P) = P;
+        next_parent(P) = P;
     }
+/*  if(first_parent(LP) == NULL){
+    first_parent(LP) = P;
+    next_parent(LP) = P;
+    prev_parent(LP) = P;
+  } else {
+    next_parent(P) = first_parent(LP);
+    prev_parent(P) = prev_parent(first_parent(LP));
+    next_parent(prev_parent(first_parent(LP))) = P;
+    prev_parent(first_parent(LP)) = P;
+    first_parent(LP) = P;
+  } */
 }
 
-void insertAfter_parent(address Prec, address P){
-    prev(next_parent(Prec))=P;
-    next_parent(P)=next_parent(Prec);
-    next_parent(Prec)=P;
-    prev(P)=Prec;
+void insertAfter_parent(List_parent &LP, address_parent Prec, address_parent P){
+    prev_parent(next_parent(Prec)) = P;
+    next_parent(P) = next_parent(Prec);
+    next_parent(Prec) = P;
+    prev_parent(P) = Prec;
 }
 
-void insertLast_parent(List &L, address P){
+void insertLast_parent(List_parent &LP, address P){ //cek kodingan di bawah coba
     address Prec;
-    Prec=first(L);
-    if (first(L)==NULL){
-        insertFirst_parent(L, P);
+    Prec = first_parent(LP);
+    if (first_parent(LP) == NULL){
+        insertFirst_parent(LP, P);
     } else {
-        while(Prec(next_parent)!=prev(first(L))){
-            Prec=next(Prec);
+        while(Prec != prev_parent(first_parent(LP))){
+            Prec = next(Prec);
         }
-        insertAfter_parent(Prec, P);
+        insertAfter_parent(LP, Prec, P);
     }
-}
-
-void deleteFirst_parent(List &L, address P){
-    P=first(L);
-    if(next_parent(P)=prev(P)) {
-        first(L)=P;
+    /*if(first_parent(LP) == NULL){
+        insertFirst_parent(LP, P);
     } else {
-        next_parent(prev(first(L)))=next_parent(P);
-        first(L)=next_parent(P);
-        next_parent(prev(first(L)))=first(L);
+        next_parent(P) = first_parent(LP);
+        prev_parent(P) = prev_parent(first_parent(LP));
+        next_parent(prev_parent(first_parent(LP))) = P;
+        prev_parent(first_parent(LP)) = P;
+    }*/
+}
+
+void deleteFirst_parent(List_parent &LP, address_parent &P){
+    P = first_parent(LP);
+    if(next_parent(P) == first_parent(LP)) {
+        next_parent(P) = NULL;
+        prev_parent(P) = NULL;
+        first_parent(LP) = NULL;
+    } else {
+        first_parent(LP) = next_parent(P);
+        prev_parent(first_parent(LP)) = prev_parent(P);
+        next_parent(prev_parent(P)) = first_parent(LP);
+        next_parent(P) = NULL;
+        prev_parent(P) = NULL;
     }
 }
 
-void deleteAfter_parent(List &L, address Prec, address P){
-    P=next_parent(Prec);
-    next_parent(Prec)=next_parent(P);
-    prev(next_parent(Prec))=Prec;
-
+void deleteAfter_parent(List_parent &LP, address_parent Prec, address_parent &P){
+    if(Prec != NULL){
+        P = next_parent(Prec);
+        next_parent(Prec) = next_parent(P);
+        prev_parent(next_parent(Prec)) = Prec;
+        next_parent(P) = NULL;
+        prev_parent(P) = NULL;
+    }
 }
 
-void deleteLast_parent(List &L, address P){
-    P=first(L);
-    if(next_parent(P)==P){
-        deleteFirst_parent(L, P);
+void deleteLast_parent(List_parent &LP, address_parent &P){
+    P = prev_parent(first_parent(LP));
+    if(P == first_parent(LP)){
+        deleteFirst_parent(LP, P);
     } else{
-        deleteAfter_parent((prev(prev(P))), Prec, P);
+        prev_parent(first_parent(LP)) = prev_parent(P);
+        next_parent(prev_parent(P)) = first_parent(LP);
+        next_parent(P) = NULL;
+        prev_parent(P) = NULL;
     }
 }
 
-void printList_parent(List &L){
-    address P;
-    P=first(L);
-    while (next_parent(P)!=prev(P)){
-        cout<<info_parent(P)<<endl;
-        P=next_parent(P);
+void printList_parent(List_parent LP){
+    address_parent P;
+    if(first_parent(LP) == NULL){
+        cout<<"============================================================================================================================================"<<endl;
+        cout<<"\t\t\t\t\t\t\t\t Daftar Mahasiswa Baru"<<endl;
+        cout<<"============================================================================================================================================"<<endl;
+        cout<<endl;
+        cout<<"\t\t\t\t\t\t\t Belum ada Mahasiswa yang terdaftar \n"<<endl;
+    } else {
+        cout<<"============================================================================================================================================"<<endl;
+        cout<<"\t\t\t\t\t\t\t\t Daftar Mahasiswa Baru"<<endl;
+        cout<<"============================================================================================================================================"<<endl;
+        cout<<"ID \t| Nama \t\t\t| Asal SMA \t\t| Jurusan SMA \t| Jenis Kelamin \t| Jalur Masuk \t| Sumbangan"<<endl;
+        cout<<"============================================================================================================================================"<<endl;
+        P = first_parent(LP);
+        do{
+            cout<<info_parent(P).ID<<"\t| "<<info_parent(P).nama<<"\t| ";
+            cout<<info_parent(P).sma<<"\t| "<<info_parent(P).jurusan_sma<<"\t\t| ";
+            cout<<info_parent(P).jenis_kelamin<<"\t\t\t| "<<info_parent(P).jalur_masuk<<"\t\t| Rp"<<info_parent(P).sumbangan;
+            cout<<endl;
+            P = next_parent(P);
+        }while(next_parent(P) != first_parent(P));
     }
 }
 
-address searchbyID_parent(List &L, int id_search){
-    address P;
-    P=first(L);
-    while(next_parent(P)!=prev(P)){
-        if(info_parent(P).ID==id_search){
-            return P;
-        }
-        P=next_parent(P);
+address searchbyID_parent(List_parent LP, int id_search){
+    address_parent P;
+    bool found;
+    if(first_parent(LP) != NULL){
+        found = false;
+        P = first_parent(LP);
+        do{
+            if(info_parent(P).ID == id_search){
+                found = true;
+            } else {
+                P = next_parent(P);
+            }
+        }while(found != true &&next_parent(P) != first_parent(LP));
     }
+    if(found != true){
+        P = NULL;
+    }
+    return P;
+}
+
+address_parent searchbyName(List_parent LP, string nama_search){
+    bool found;
+    address_parent P;
+    if(first_parent(LP) != NULL){
+        found = false;
+        P = first_parent(LP);
+        do{
+            if(info_parent(P).nama == nama_search){
+                found = true;
+            } else {
+                P = next_parent(P);
+            }
+        }while(found != true && next_parent(P) != first_parent(LP));
+    }
+    if(found != true){
+        P = NULL;
+    }
+    return P;
 }
--- list_parent.h
+++ list_parent.h
@@ -1,41 +1,48 @@
 #ifndef LIST_PARENT_H_INCLUDED
 #define LIST_PARENT_H_INCLUDED
+
 #define info_parent(P) P->info_parent
 #define next_parent(P) P->next_parent
-#define prev(P) P->prev
-#define first(L) L.first
+#define prev_parent(P) P->prev_parent
+#define first_parent(L) L.first_parent
+#define ke_parent(P) P->parent
+
 #include <iostream>
 
 using namespace std;
 
-typedef infotype {
+struct parent {
     int ID;
-    string nama, sma;
-    string jalur_masuk;
-    string jurusan_sma;
+    string nama, sma, jenis_kelamin, jalur_masuk, jurusan_sma;
     int sumbangan;
-
-}
-typedef struct *elmList address;
-struct elmList{
-    infotype info_parent;
-    address next_parent;
-    address prev;
 };
 
-struct List{
-    address first;
+typedef struct elmList_parent *address_parent;
+
+struct elmList_parent{
+    parent info_parent;
+    address_parent next_parent;
+    address_parent prev_parent;
 };
 
-void createList_parent(List &L);
-void newElement_parent(address &P, int id_baru, string nama_mhs, string asal_sekolah, string major, int donasi);
-void insertFirst_parent(List &L, address P);
-void insertAfter_parent(address Prec, address P);
-void insertLast_parent(List &L, address P);
-void deleteFirst_parent(List &L, address P);
-void deleteAfter_parent(List &L, address Prec, address P);
-void deleteLast_parent(List &L, address P);
-void printList_parent(List &L);
-address searchbyID_parent(List &L, int id_search);
+struct List_parent{
+    address_parent first_parent;
+};
+
+void createList_parent(List_parent &LP);
+address_parent newElement_parent(parent x);
+void deallocate_parent(address_parent &P);
+address_parent input_parent();
+void insertFirst_parent(List_parent &LP, address_parent P);
+void insertAfter_parent(List_parent &LP, address_parent Prec, address_parent P);
+void insertLast_parent(List_parent &LP, address_parent P);
+void deleteFirst_parent(List_parent &LP, address_parent &P);
+void deleteAfter_parent(List_parent &LP, address_parent Prec, address_parent &P);
+void deleteLast_parent(List_parent &LP, address_parent &P);
+void printList_parent(List_parent LP);
+address_parent searchbyID_parent(List_parent &LP, int id_search);
+address_parent searchbyName(List_parent LP, string nama_search);
+void editData_parent(List_parent LP, address_parent &P);
+void deleteSearch_parent(List_parent &LP, address_parent &P);
 
 #endif // LIST_PARENT_H_INCLUDED
